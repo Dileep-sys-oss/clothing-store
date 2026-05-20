50b2db6adcd231a99ef1f7f0eed2b55c0fc806c7
Dileep H D <dileep.hd@nagra.com>
2026-05-20 23:35:07 +0530

    Bump asset cache version

--- docs/js/ui.js
+++ docs/js/ui.js
@@ -19,7 +19,7 @@ async function loadProducts() {
     return cachedProducts;
   }
 
-  const response = await fetch("data/products.json?v=20260520");
+  const response = await fetch("data/products.json?v=20260520-2");
   const baseProducts = await response.json();
   const adminOverride = getAdminProducts();
   cachedProducts = adminOverride || baseProducts;
